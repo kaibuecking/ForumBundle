1aae5f3283b8aeaf8d963a436e95e25c7e642a3f
Kai Bücking <kai.buecking@yahoo.de>
2024-11-30 12:23:08 +0100

    Update C4GForumHelper.php

--- src/Classes/C4GForumHelper.php
+++ src/Classes/C4GForumHelper.php
@@ -1576,7 +1576,7 @@ class C4GForumHelper extends \System
 
                 break;
         }
-        $select = 'SELECT a.id,a.pid AS threadid,' . $sqlAuthor . ',a.author AS authorid,a.creation,a.subject,a.text,c.name AS threadname, c.author AS threadauthor, d.name AS forumname,d.id AS forumid, a.rating, ' .
+        $select = 'SELECT \'first\', a.id,a.pid AS threadid,' . $sqlAuthor . ',a.author AS authorid,a.creation,a.subject,a.text,c.name AS threadname, c.author AS threadauthor, d.name AS forumname,d.id AS forumid, a.rating, ' .
                          'a.post_number, c.posts, a.edit_count, ' . $sqlEditUser . ' AS edit_username, a.edit_last_time, a.linkname, a.linkurl, d.link_newwindow,' .
                          'a.loc_geox, a.loc_geoy, a.loc_data_type, a.loc_data_content, a.locstyle, a.loc_label, a.loc_tooltip, a.loc_osm_id, a.tags, ' .
                          'd.map_label, d.map_tooltip, d.map_popup, d.map_link ' .
@@ -1585,9 +1585,9 @@ class C4GForumHelper extends \System
                 'INNER JOIN tl_c4g_forum_thread c ON c.id = a.pid ' .
                 'INNER JOIN tl_c4g_forum d ON d.id = c.pid ' .
                 'LEFT JOIN tl_member e ON e.id = a.edit_last_author' .
-                'WHERE a.post_number = 1' .
+                'WHERE WHERE a.pid = ? AND a.post_number = 1' .
                 'UNION ALL';
-        $select .= 'SELECT a.id,a.pid AS threadid,' . $sqlAuthor . ',a.author AS authorid,a.creation,a.subject,a.text,c.name AS threadname, c.author AS threadauthor, d.name AS forumname,d.id AS forumid, a.rating, ' .
+        $select .= 'SELECT \'follow\', a.id,a.pid AS threadid,' . $sqlAuthor . ',a.author AS authorid,a.creation,a.subject,a.text,c.name AS threadname, c.author AS threadauthor, d.name AS forumname,d.id AS forumid, a.rating, ' .
                          'a.post_number, c.posts, a.edit_count, ' . $sqlEditUser . ' AS edit_username, a.edit_last_time, a.linkname, a.linkurl, d.link_newwindow,' .
                          'a.loc_geox, a.loc_geoy, a.loc_data_type, a.loc_data_content, a.locstyle, a.loc_label, a.loc_tooltip, a.loc_osm_id, a.tags, ' .
                          'd.map_label, d.map_tooltip, d.map_popup, d.map_link ' .
@@ -1599,12 +1599,12 @@ class C4GForumHelper extends \System
 
         if ($threadId <> 0) {
             $posts = $this->Database->prepare(
-                $select . ' WHERE a.pid = ? AND a.post_number != 1 ORDER BY a.id ' . $order)
-                ->execute($threadId);
+                $select . ' WHERE a.pid = ? AND a.post_number != 1 ORDER BY 1, a.id ' . $order)
+                ->execute($threadId,$threadId);
         } else {
             $posts = $this->Database->prepare(
-                $select . ' WHERE a.id = ? AND a.post_number != 1 ')
-                ->execute($postId);
+                $select . ' WHERE a.id = ? AND a.post_number != 1 ORDER BY 1')
+                ->execute($postId, $postId);
         }
         $aPosts = $posts->fetchAllAssoc();
 
